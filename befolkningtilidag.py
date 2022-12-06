4a46b19d99df4aa69407eddbac974860069b2906
abo004 <92145442+abo004@users.noreply.github.com>
2022-12-06 11:37:22 +0000

    hello

---
+++ befolkningtilidag.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import csv
+
+filnavn = 'Befolkning.csv'
+
+aarstall = []
+befolkning = []
+
+with open(filnavn, encoding="utf-8-sig") as fil:
+    filinnhold = csv.reader(fil, delimiter=";")
+
+    overskrifter = next(filinnhold)
+    print(overskrifter)
+
+    for rad in filinnhold:
+        aarstall.append(int(rad[0]))
+        befolkning.append(int(rad[1]))
+
+# Tegner grafen
+plt.plot(aarstall, befolkning)
+plt.grid()
+plt.show()
